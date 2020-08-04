d5f6bbfc2b3a195bede946e0fccd713539e32580
ghazalb76 <ghazal.b93@gmail.com>
2020-08-04 20:30:14 +0430

    Add all source files

---
+++ src/main/java/com/example/demo/GrpcConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import ir.tourismit.tit.core.configuration.GrpcChannel;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is just for config the Grpc client for GrpcTest controller
+ */
+@Component
+public class GrpcConfig extends GrpcChannel {
+}
---
+++ src/main/java/com/example/demo/testController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import ir.tourismit.tit.core.Request;
+import ir.tourismit.tit.core.Response;
+import ir.tourismit.tit.core.grpc.GrpcClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @PostMapping("/request")
+    public ResponseEntity<String> callService() {
+        Request request1 = Request.newBuilder()
+                .setProcessID("request.getProcessId()")
+                .setProcessReference("request.getProcessReference()")
+                .setCallerReference("request.getCallerReference()")
+                .setServiceID("request.getServiceId()")
+                .setCallerInputJson("request.getCallerInputJson()")
+                .setPreviewsOutput("request.getPreviewsOutput()")
+                .setTokenAA("request.getTokenAA()")
+                .build();
+        System.out.println("Request");
+        System.out.println(request1);
+        Response response = GrpcClient.getInstance().service(request1);
+        System.out.println("Response");
+        System.out.println(response);
+
+        return new ResponseEntity<>(response.getOutputJson(), HttpStatus.OK);
+    }
+}
